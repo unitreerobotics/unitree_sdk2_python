d801b12fac93841c5d31c3ade3892c1e2ffc78d9
jiayun.sun <rd_sjy@unitree.com>
2026-05-11 20:47:12 +0800

    add init py

--- unitree_sdk2py/__init__.py
+++ unitree_sdk2py/__init__.py
@@ -1,10 +1,14 @@
-from . import idl, utils, core, rpc, go2, b2
+from . import idl, utils, core, rpc, comm, go2, b2, g1, h1, h2
 
 __all__ = [
     "idl",
     "utils",
     "core",
     "rpc",
+    "comm",
     "go2",
     "b2",
+    "g1",
+    "h1",
+    "h2",
 ]
